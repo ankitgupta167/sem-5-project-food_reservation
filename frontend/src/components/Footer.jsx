3b33ffe3e07b867f89f49136b88ca4e8131b52d6
ankitgupta167 <ankitpgupta051@gmail.com>
2025-08-27 20:03:10 +0530

    changing

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="banner">
           <div className="left">ANKIT</div>
           <div className="right">
-            <p>Balalma apt, Quees park, Mira-road, Thane</p>
+            <p>Balalma apt, Queens park, Mira-road, Thane</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
